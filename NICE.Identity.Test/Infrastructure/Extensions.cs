68cb4f54d8c658ae00d6b8790fa4f7d8921da0cc
John Williams <jcwilliams@gmail.com>
2019-01-10 09:39:25 +0000

    fixing the vsdiffmerge fallback diffing tool path.

--- NICE.Identity.Test/Infrastructure/Extensions.cs
+++ NICE.Identity.Test/Infrastructure/Extensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace NICE.Identity.Test.Infrastructure
@@ -75,7 +76,7 @@ namespace NICE.Identity.Test.Infrastructure
 			else
 			{
 				var dir = Directory.GetCurrentDirectory();
-				var vsDiffPath = Path.GetFullPath(Path.Combine(dir, "Difftools", "vsDiffMerge.exe"));
+				var vsDiffPath = Path.GetFullPath(Path.Combine(dir, "../../../Difftools", "vsDiffMerge.exe"));
 				Process.Start(vsDiffPath, string.Format("\"{0}\" \"{1}\" /t", expectedFilePath, actualFilePath));
 			}
 
