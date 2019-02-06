6359e65cada3bd5183926eb480c8df43a1772093
John Williams <jcwilliams@gmail.com>
2019-02-06 15:19:45 +0000

    IDAM-100 - renaming role management webapi project to NICE.Identity.Authorisation.WebAPI also making the project build correctly.

--- NICE.Identity.ManagementPortal/Migrations/20190117105036_InitialCreate.cs
+++ NICE.Identity.ManagementPortal/Migrations/20190117105036_InitialCreate.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace NICE.Identity.Migrations
+namespace z_deprecated_NICE.Identity.Migrations
 {
     public partial class InitialCreate : Migration
     {
