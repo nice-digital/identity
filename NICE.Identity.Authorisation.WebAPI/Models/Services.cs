a579463e0845cc738acffdb56d0f7604776fdecf
John Williams <jcwilliams@gmail.com>
2019-02-11 13:16:08 +0000

    IDAM-115- generating db models from the schema

---
+++ NICE.Identity.Authorisation.WebAPI/Models/Services.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NICE.Identity.Authorisation.WebAPI.Models
+{
+    public partial class Services
+    {
+        public Services()
+        {
+            Websites = new HashSet<Websites>();
+        }
+
+        public int ServiceId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Websites> Websites { get; set; }
+    }
+}
