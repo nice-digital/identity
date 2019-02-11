4dd82722b8d15b1b03eed837b89f24322cfb4b52
John Williams <jcwilliams@gmail.com>
2019-02-11 16:26:47 +0000

    IDAM-107- adding tests around retrieving claims

---
+++ NICE.Identity.Test/UnitTests/ClaimsServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NICE.Identity.Authorisation.WebAPI.Services;
+using NICE.Identity.Test.Infrastructure;
+using Shouldly;
+using System;
+using System.Linq;
+using NICE.Identity.Authorisation.WebAPI.Models.Responses;
+using Xunit;
+
+namespace NICE.Identity.Test.UnitTests
+{
+	public class ClaimsServiceTests : TestBase
+	{
+		[Fact]
+		public void UserIsReturnedInClaims()
+		{
+			//Arrange
+			var context = GetContext();
+			TestData.AddWithTwoRoles(ref context);
+			var claimsService = new ClaimsService(context);
+
+			//Act
+			var claims = claimsService.GetClaims(1);
+
+			//Assert
+			claims.Single(claim => claim.Type.Equals(ClaimType.FirstName)).Value.ShouldBe("Steve");
+		}
+
+
+	}
+}
