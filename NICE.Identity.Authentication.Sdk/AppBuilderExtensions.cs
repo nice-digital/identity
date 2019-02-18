68100a7b68c487bbe43ffedce7b50cea3319eff1
John Williams <jcwilliams@gmail.com>
2019-02-18 14:45:55 +0000

    IDAM-154 - framwork client working, but not pretty. needs a refactor.

---
+++ NICE.Identity.Authentication.Sdk/AppBuilderExtensions.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace NICE.Identity.Authentication.Sdk
+{
+	public static class AppBuilderExtensions
+	{
+		public static void AddAuthentication(this IAppBuilder app, NameValueCollection appSettings)
+		{
+			AddAuthentication(app, appSettings["Domain"], appSettings["ClientId"], appSettings["ClientSecret"], appSettings["RedirectUri"], appSettings["PostLogoutRedirectUri"]);
+		}
+
+		public static void AddAuthentication(this IAppBuilder app, string domain, string clientId, string clientSecret, string redirectURI, string postLogoutRedirectURI)
+		{
+
+			// Enable Kentor Cookie Saver middleware
+			app.UseKentorOwinCookieSaver();
+
+			// Set Cookies as default authentication type
+			app.SetDefaultSignInAsAuthenticationType(Microsoft.Owin.Security.Cookies.CookieAuthenticationDefaults.AuthenticationType);
+			app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+			{
+				AuthenticationType = Microsoft.Owin.Security.Cookies.CookieAuthenticationDefaults.AuthenticationType,
+				LoginPath = new Microsoft.Owin.PathString("/Account/Login")
+			});
+
+			// Configure Auth0 authentication
+			app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+			{
+				AuthenticationType = "Auth0",
+
+				Authority = $"https://{domain}",
+
+				ClientId = clientId,
+				ClientSecret = clientSecret,
+
+				RedirectUri = redirectURI,
+				PostLogoutRedirectUri = postLogoutRedirectURI,
+
+				ResponseType = OpenIdConnectResponseType.CodeIdToken,
+				Scope = "openid profile",
+
+				TokenValidationParameters = new TokenValidationParameters
+				{
+					NameClaimType = "name"
+				},
+
+				Notifications = new OpenIdConnectAuthenticationNotifications
+				{
+					RedirectToIdentityProvider = notification =>
+					{
+						if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
+						{
+							var logoutUri = $"https://{domain}/v2/logout?client_id={clientId}";
+
+							var postLogoutUri = notification.ProtocolMessage.PostLogoutRedirectUri;
+							if (!string.IsNullOrEmpty(postLogoutUri))
+							{
+								if (postLogoutUri.StartsWith("/"))
+								{
+									// transform to absolute
+									var request = notification.Request;
+									postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+								}
+								logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+							}
+
+							notification.Response.Redirect(logoutUri);
+							notification.HandleResponse();
+						}
+						return Task.FromResult(0);
+					}
+				}
+			});
+
+		}
+	}
+}
--- TestClients/NICE.Identity.TestClient.NETFramework/Startup.cs
+++ TestClients/NICE.Identity.TestClient.NETFramework/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Newtonsoft.Json.Linq;
+using NICE.Identity.Authentication.Sdk;
 using Owin;
 
 [assembly: OwinStartup(typeof(NICE.Identity.TestClient.NETFramework.Startup))]
@@ -26,71 +27,13 @@ namespace NICE.Identity.TestClient.NETFramework
 
 			// Configure Auth0 parameters
 			string auth0Domain = secretsFile.SelectToken("Auth0")["Domain"].ToString();
-			string auth0ClientId = secretsFile.SelectToken("Auth0")["ClientId"].ToString(); 
-			string auth0ClientSecret = secretsFile.SelectToken("Auth0")["ClientSecret"].ToString(); 
-			string auth0RedirectUri = secretsFile.SelectToken("Auth0")["RedirectUri"].ToString(); 
-			string auth0PostLogoutRedirectUri = secretsFile.SelectToken("Auth0")["PostLogoutRedirectUri"].ToString(); 
+			string auth0ClientId = secretsFile.SelectToken("Auth0")["ClientId"].ToString();
+			string auth0ClientSecret = secretsFile.SelectToken("Auth0")["ClientSecret"].ToString();
+			string auth0RedirectUri = secretsFile.SelectToken("Auth0")["RedirectUri"].ToString();
+			string auth0PostLogoutRedirectUri = secretsFile.SelectToken("Auth0")["PostLogoutRedirectUri"].ToString();
 
-			// Enable Kentor Cookie Saver middleware
-			app.UseKentorOwinCookieSaver();
-
-			// Set Cookies as default authentication type
-			app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
-			app.UseCookieAuthentication(new CookieAuthenticationOptions
-			{
-				AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
-				LoginPath = new PathString("/Account/Login")
-			});
-
-			// Configure Auth0 authentication
-			app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
-			{
-				AuthenticationType = "Auth0",
-
-				Authority = $"https://{auth0Domain}",
-
-				ClientId = auth0ClientId,
-				ClientSecret = auth0ClientSecret,
-
-				RedirectUri = auth0RedirectUri,
-				PostLogoutRedirectUri = auth0PostLogoutRedirectUri,
-
-				ResponseType = OpenIdConnectResponseType.CodeIdToken,
-				Scope = "openid profile",
-
-				TokenValidationParameters = new TokenValidationParameters
-				{
-					NameClaimType = "name"
-				},
-
-				Notifications = new OpenIdConnectAuthenticationNotifications
-				{
-					RedirectToIdentityProvider = notification =>
-					{
-						if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
-						{
-							var logoutUri = $"https://{auth0Domain}/v2/logout?client_id={auth0ClientId}";
-
-							var postLogoutUri = notification.ProtocolMessage.PostLogoutRedirectUri;
-							if (!string.IsNullOrEmpty(postLogoutUri))
-							{
-								if (postLogoutUri.StartsWith("/"))
-								{
-									// transform to absolute
-									var request = notification.Request;
-									postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-								}
-								logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
-							}
-
-							notification.Response.Redirect(logoutUri);
-							notification.HandleResponse();
-						}
-						return Task.FromResult(0);
-					}
-				}
-			});
-
+			app.AddAuthentication(auth0Domain, auth0ClientId, auth0ClientSecret, auth0RedirectUri, auth0PostLogoutRedirectUri);
+			
 		}
 	}
 }
