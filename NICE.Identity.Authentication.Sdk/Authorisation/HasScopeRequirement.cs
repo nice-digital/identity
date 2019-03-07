a7965d86a90e84e84f64a6c1a9c964b30dd5f24a
John Williams <jcwilliams@gmail.com>
2019-03-07 08:52:16 +0000

    IDAM-194 - removing dependence on hard-coded role names.

---
+++ NICE.Identity.Authentication.Sdk/Authorisation/HasScopeRequirement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NICE.Identity.Authentication.Sdk.Authorisation
+{
+	public class HasScopeRequirement : IAuthorizationRequirement
+	{
+		public string Issuer { get; }
+		public string Scope { get; }
+
+		public HasScopeRequirement(string scope, string issuer)
+		{
+			Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+			Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+		}
+	}
+}
