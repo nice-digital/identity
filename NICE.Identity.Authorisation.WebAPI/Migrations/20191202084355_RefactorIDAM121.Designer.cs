8800fcfbe332ed8029a12198a36f2f02931f85d9
John Williams <jcwilliams@gmail.com>
2019-12-02 08:44:50 +0000

    IDAM-121 -  migration to match the bit flag refactor

--- NICE.Identity.Authorisation.WebAPI/Migrations/20191202084355_RefactorIDAM121.Designer.cs
+++ NICE.Identity.Authorisation.WebAPI/Migrations/20191202084355_RefactorIDAM121.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NICE.Identity.Authorisation.WebAPI.Repositories;
 
 namespace NICE.Identity.Authorisation.WebAPI.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20191202084355_RefactorIDAM121")]
+    partial class RefactorIDAM121
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NICE.Identity.Authorisation.WebAPI/Migrations/20191202084355_RefactorIDAM121.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NICE.Identity.Authorisation.WebAPI.Migrations
+{
+    public partial class RefactorIDAM121 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsStaffMember",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMigrated",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsLockedOut",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasVerifiedEmailAddress",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "AllowContactMe",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsStaffMember",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMigrated",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsLockedOut",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasVerifiedEmailAddress",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "AllowContactMe",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(bool));
+        }
+    }
+}
