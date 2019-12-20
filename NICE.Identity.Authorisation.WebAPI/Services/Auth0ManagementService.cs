408da0a1043d99fb40ca215494f69a7af4e99664
John Williams <jcwilliams@gmail.com>
2019-12-20 16:34:02 +0000

    IDAM-325 - updating revoke code. wrong application selected.

--- NICE.Identity.Authorisation.WebAPI/Services/Auth0ManagementService.cs
+++ NICE.Identity.Authorisation.WebAPI/Services/Auth0ManagementService.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Auth0.AuthenticationApi;
 using Auth0.AuthenticationApi.Models;
 using Auth0.ManagementApi;
 using Auth0.ManagementApi.Models;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using NICE.Identity.Authentication.Sdk.Domain;
 using NICE.Identity.Authorisation.WebAPI.Configuration;
 using User = NICE.Identity.Authorisation.WebAPI.DataModels.User;
 
@@ -13,10 +20,12 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
     public class Auth0ManagementService : IProviderManagementService
     {
         private readonly ILogger<Auth0ManagementService> _logger;
-        
-        public Auth0ManagementService(ILogger<Auth0ManagementService> logger)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public Auth0ManagementService(ILogger<Auth0ManagementService> logger, IHttpClientFactory httpClientFactory)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+	        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+	        _httpClientFactory = httpClientFactory;
         }
 
         public async Task<string> GetAccessTokenForManagementAPI()
@@ -86,16 +95,42 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
             }
         }
 
+        private async Task RevokeRefreshToken(string refreshToken)
+        {
+	        var client = _httpClientFactory.CreateClient();
+            var uri = new Uri($"https://{AppSettings.ManagementAPI.Domain}/oauth/revoke");
+
+	        var postObject = new
+	        {
+		        client_id = AppSettings.ManagementAPI.ClientId, //TODO: this is the wrong application. 
+		        client_secret = AppSettings.ManagementAPI.ClientSecret, //and secret.
+		        token = refreshToken
+	        };
+
+	        var request = new HttpRequestMessage(HttpMethod.Post, uri)
+	        {
+		        Content = new StringContent(JsonConvert.SerializeObject(postObject), Encoding.UTF8, "application/json")
+	        };
+	        var responseMessage = await client.SendAsync(request);
+	        if (!responseMessage.IsSuccessStatusCode)
+	        {
+                throw new Exception($"Error {(int) responseMessage.StatusCode} trying to revoke refresh token: {refreshToken}");
+	        }
+        }
+
         public async Task RevokeRefreshTokensForUser(string nameIdentifier)
         {
 	        var managementApiAccessToken = await GetAccessTokenForManagementAPI();
 	        var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain);
 
+            //annoyingly we can't use the device credential api, as it's rate limited, but doesn't return the X-RateLimit-Remaining header so, is basically unusable.
 	        var allDevices = await managementApiClient.DeviceCredentials.GetAllAsync(userId: nameIdentifier);
 
-	        foreach (var device in allDevices)
+	        var firstRefreshToken = allDevices.FirstOrDefault(device => device.Type.Equals("refresh_token"))?.Id;
+
+	        if (!string.IsNullOrEmpty(firstRefreshToken))
 	        {
-		        await managementApiClient.DeviceCredentials.DeleteAsync(device.DeviceId);
+		        await RevokeRefreshToken(firstRefreshToken);
 	        }
         }
     }
