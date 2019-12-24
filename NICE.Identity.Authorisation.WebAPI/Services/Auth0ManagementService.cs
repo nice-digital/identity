0443dc5fd3a954a36c5730f0a6d510b5f9770e36
John Williams <jcwilliams@gmail.com>
2019-12-24 14:29:48 +0000

    IDAM-325 - adding in some simple rate limiting support when calling the management api.

--- NICE.Identity.Authorisation.WebAPI/Services/Auth0ManagementService.cs
+++ NICE.Identity.Authorisation.WebAPI/Services/Auth0ManagementService.cs
@@ -1,18 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Auth0.AuthenticationApi;
 using Auth0.AuthenticationApi.Models;
 using Auth0.ManagementApi;
 using Auth0.ManagementApi.Models;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using NICE.Identity.Authentication.Sdk.Domain;
 using NICE.Identity.Authorisation.WebAPI.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using User = NICE.Identity.Authorisation.WebAPI.DataModels.User;
 
 namespace NICE.Identity.Authorisation.WebAPI.Services
@@ -20,19 +17,19 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
     public class Auth0ManagementService : IProviderManagementService
     {
         private readonly ILogger<Auth0ManagementService> _logger;
-        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
 
         public Auth0ManagementService(ILogger<Auth0ManagementService> logger, IHttpClientFactory httpClientFactory)
         {
 	        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-	        _httpClientFactory = httpClientFactory;
+	        _httpClient = httpClientFactory.CreateClient();
         }
 
         public async Task<string> GetAccessTokenForManagementAPI()
         {
 	        try
 	        {
-                var client = new AuthenticationApiClient(new Uri($"https://{AppSettings.ManagementAPI.Domain}/"));
+                var client = new AuthenticationApiClient(new Uri($"https://{AppSettings.ManagementAPI.Domain}/"), _httpClient);
 		        var managementApiTokenRequest = new ClientCredentialsTokenRequest
 		        {
 			        ClientId = AppSettings.ManagementAPI.ClientId,
@@ -56,7 +53,7 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
             _logger.LogInformation($"Update user {authenticationProviderUserId} in auth0");
 
             var managementApiAccessToken = await GetAccessTokenForManagementAPI();
-            var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain);
+            var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain, _httpClient);
             try
             {
                 var userUpdateRequest = new UserUpdateRequest
@@ -82,7 +79,7 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
             _logger.LogInformation($"Delete user {authenticationProviderUserId} from auth0");
 
             var managementApiAccessToken = await GetAccessTokenForManagementAPI();
-            var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain);
+            var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain, _httpClient);
 
             try
             {
@@ -95,43 +92,56 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
             }
         }
 
-        private async Task RevokeRefreshToken(string refreshToken)
+        public async Task RevokeRefreshTokensForUser(string nameIdentifier)
         {
-	        var client = _httpClientFactory.CreateClient();
-            var uri = new Uri($"https://{AppSettings.ManagementAPI.Domain}/oauth/revoke");
 
-	        var postObject = new
-	        {
-		        client_id = AppSettings.ManagementAPI.ClientId, //TODO: this is the wrong application. 
-		        client_secret = AppSettings.ManagementAPI.ClientSecret, //and secret.
-		        token = refreshToken
-	        };
+	        _logger.LogInformation($"Revoke Refresh Tokens For User {nameIdentifier}");
 
-	        var request = new HttpRequestMessage(HttpMethod.Post, uri)
+            var managementApiAccessToken = await GetAccessTokenForManagementAPI();
+
+            var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain, _httpClient);
+
+	        var allDevices = await managementApiClient.DeviceCredentials.GetAllAsync(userId: nameIdentifier, type: "refresh_token");
+
+	        if (allDevices.Any())
 	        {
-		        Content = new StringContent(JsonConvert.SerializeObject(postObject), Encoding.UTF8, "application/json")
-	        };
-	        var responseMessage = await client.SendAsync(request);
-	        if (!responseMessage.IsSuccessStatusCode)
-	        {
-                throw new Exception($"Error {(int) responseMessage.StatusCode} trying to revoke refresh token: {refreshToken}");
+		        RevokeRefreshToken(managementApiClient, allDevices.Select(device => device.Id)); //intentionally not awaiting. this might take a while to complete due to rate limiting.
 	        }
         }
 
-        public async Task RevokeRefreshTokensForUser(string nameIdentifier)
+        private async Task RevokeRefreshToken(ManagementApiClient managementApiClient, IEnumerable<string> refreshTokens)
         {
-	        var managementApiAccessToken = await GetAccessTokenForManagementAPI();
-	        var managementApiClient = new ManagementApiClient(managementApiAccessToken, AppSettings.ManagementAPI.Domain);
+	        _logger.LogInformation($"Starting Revoking {refreshTokens.Count()} Refresh Tokens");
 
-            //annoyingly we can't use the device credential api, as it's rate limited, but doesn't return the X-RateLimit-Remaining header so, is basically unusable.
-	        var allDevices = await managementApiClient.DeviceCredentials.GetAllAsync(userId: nameIdentifier);
-
-	        var firstRefreshToken = allDevices.FirstOrDefault(device => device.Type.Equals("refresh_token"))?.Id;
-
-	        if (!string.IsNullOrEmpty(firstRefreshToken))
+            foreach (var refreshToken in refreshTokens)
 	        {
-		        await RevokeRefreshToken(firstRefreshToken);
+				await managementApiClient.DeviceCredentials.DeleteAsync(refreshToken); //when we hit the management api too frequently in a short space of time (~ > 10 requests a second), you get a http status 429 error - "too many requests" 
+                _logger.LogInformation($"Refresh token: {refreshToken} revoked");
+
+				var rateLimit = managementApiClient.GetLastApiInfo().RateLimit; //this object is set by the headers returned when the DeleteAsync method is hit.
+				var utcNow = DateTime.UtcNow;
+				var rateLimitResetTimeUtc = rateLimit.Reset.ToUniversalTime();
+				var rateLimitCounter = 0;
+                if (rateLimit.Remaining <= 1 && rateLimitResetTimeUtc > utcNow)
+                {
+	                rateLimitCounter++;
+                    if (rateLimitCounter > 100) //loop limit here just to be safe, as this is going to execute in the background. this is going to limit revoking a maximum of ~1000 refresh tokens, which seems more than enough.
+	                {
+		                _logger.LogError($"Rate limiting looped 100 times. this seems way too high. refresh token count: {refreshToken.Length}");
+                        break;
+	                }
+	                var timeSpanToSleep = utcNow - rateLimitResetTimeUtc;
+	                if (timeSpanToSleep.TotalMinutes > 10) //typically it'll be a few seconds delay
+	                {
+		                _logger.LogError($"Rate limiting returned a time in excess of 10 minutes. this seems way too high. refresh token count: {refreshToken.Length}");
+                        break;
+	                }
+	                _logger.LogWarning($"Rate limiting the refresh token revocation. sleep time in seconds: {timeSpanToSleep.TotalSeconds}");
+                    Thread.Sleep(timeSpanToSleep);
+                }
 	        }
+
+            _logger.LogInformation($"Finished Revoking {refreshTokens.Count()} Refresh Tokens");
         }
     }
 }
