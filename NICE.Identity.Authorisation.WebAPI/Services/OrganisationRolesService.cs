2a22e6eaa93e3148f26d5a63ab8143e855b54f14
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-07-28 11:36:10 +0100

    EC-838 Added get and delete for organisation roles

--- NICE.Identity.Authorisation.WebAPI/Services/OrganisationRolesService.cs
+++ NICE.Identity.Authorisation.WebAPI/Services/OrganisationRolesService.cs
@@ -11,6 +11,9 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
     public interface IOrganisationRolesService
     {
         OrganisationRole CreateOrganisationRole(OrganisationRole organisationRole);
+        List<OrganisationRole> GetOrganisationRoles();
+        OrganisationRole GetOrganisationRole(int organisationRoleId);
+        int DeleteOrganisationRole(int organisationRoleId);
     }
 
     public class OrganisationRolesService : IOrganisationRolesService
@@ -39,6 +42,34 @@ namespace NICE.Identity.Authorisation.WebAPI.Services
                 throw new Exception($"Failed to create organisation role for {organisationRole.OrganisationId.ToString()} - exception: {e.Message}");
             }
 
+        }
+
+        public List<OrganisationRole> GetOrganisationRoles()
+        {
+            return _context.OrganisationRoles.Select(organisationRole => new OrganisationRole(organisationRole)).ToList();
+        }
+
+        public OrganisationRole GetOrganisationRole(int organisationRoleId)
+        {
+            var organisationRole = _context.OrganisationRoles.Where((o => o.OrganisationRoleId == organisationRoleId)).FirstOrDefault();
+            return organisationRole != null ? new OrganisationRole(organisationRole) : null;
+        }
+
+        public int DeleteOrganisationRole(int organisationRoleId)
+        {
+            try
+            {
+                var organisationRoleToDelete = _context.OrganisationRoles.Find(organisationRoleId);
+                if (organisationRoleToDelete == null)
+                    return 0;
+                _context.OrganisationRoles.RemoveRange(organisationRoleToDelete);
+                return _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to delete organisation role {organisationRoleId.ToString()} - exception: {e.Message}");
+                throw new Exception($"Failed to delete organisation role {organisationRoleId.ToString()} - exception: {e.Message}");
+            }
         }
     }
 
--- NICE.Identity.Test/UnitTests/Authorisation.WebAPI/Services/OrganisationRolesServiceTests.cs
+++ NICE.Identity.Test/UnitTests/Authorisation.WebAPI/Services/OrganisationRolesServiceTests.cs
@@ -46,5 +46,121 @@ namespace NICE.Identity.Test.UnitTests.Authorisation.WebAPI.Services
             createdOrganisationRole.RoleId.ShouldBe(1);
         }
 
+        [Fact]
+        public void Get_organisation_roles()
+        {
+            //Arrange
+            var context = GetContext();
+            var organisationRolesService = new OrganisationRolesService(context, _logger.Object);
+            organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 1,
+                OrganisationId = 1,
+                RoleId = 1
+            });
+            organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 2,
+                OrganisationId = 2,
+                RoleId = 2
+            });
+
+            //Act
+            var organisationRoles = organisationRolesService.GetOrganisationRoles();
+
+            //Assert
+            organisationRoles.Count.ShouldBe(2);
+            organisationRoles[0].OrganisationId.ShouldBe(1);
+            organisationRoles[1].OrganisationId.ShouldBe(2);
+        }
+
+        [Fact]
+        public void Get_organisation_role()
+        {
+            //Arrange
+            var context = GetContext();
+            var organisationRolesService = new OrganisationRolesService(context, _logger.Object);
+            var createdOrganisationRole = organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 1,
+                OrganisationId = 1,
+                RoleId = 1
+            });
+
+            //Act
+            var organisationRole = organisationRolesService.GetOrganisationRole(createdOrganisationRole.OrganisationRoleId.GetValueOrDefault());
+
+            //Assert
+            context.OrganisationRoles.Count().ShouldBe(1);
+            organisationRole.OrganisationId.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Get_organisation_role_that_does_not_exist()
+        {
+            //Arrange
+            var context = GetContext();
+            var organisationRolesService = new OrganisationRolesService(context, _logger.Object);
+            organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 1,
+                OrganisationId = 1,
+                RoleId = 1
+            });
+
+            //Act
+            var organisationRole = organisationRolesService.GetOrganisationRole(9999);
+
+            //Assert
+            organisationRole.ShouldBeNull();
+        }
+
+        [Fact]
+        public void Delete_organisation_role()
+        {
+            //Arrange
+            var context = GetContext();
+            var organisationRolesService = new OrganisationRolesService(context, _logger.Object);
+            var createdOrganisationRole = organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 1,
+                OrganisationId = 1,
+                RoleId = 1
+            });
+            organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 2,
+                OrganisationId = 2,
+                RoleId = 2
+            });
+
+            //Act
+            var deletedOrganisationRoleResponse = organisationRolesService.DeleteOrganisationRole(createdOrganisationRole.OrganisationRoleId.GetValueOrDefault());
+
+            //Assert
+            deletedOrganisationRoleResponse.ShouldBe(1);
+            context.OrganisationRoles.Count().ShouldBe(1);
+        }
+
+        [Fact]
+        public void Delete_organisation_role_that_does_not_exist()
+        {
+            //Arrange
+            var context = GetContext();
+            var organisationRolesService = new OrganisationRolesService(context, _logger.Object);
+            organisationRolesService.CreateOrganisationRole(new ApiModels.OrganisationRole()
+            {
+                OrganisationRoleId = 1,
+                OrganisationId = 1,
+                RoleId = 1
+            });
+
+            //Act
+            var deletedOrganisationRoleResponse = organisationRolesService.DeleteOrganisationRole(9999);
+
+            //Assert
+            deletedOrganisationRoleResponse.ShouldBe(0);
+            context.OrganisationRoles.Count().ShouldBe(1);
+        }
     }
 }
