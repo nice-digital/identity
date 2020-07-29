fad9b58e9204db79d14232e5da727c19108110ab
Sue Garner <Sue.Garner@nice.org.uk>
2020-07-29 11:59:11 +0100

    EC-838 Added API endpoints for Organisation

--- NICE.Identity.Authorisation.WebAPI/Controllers/OrganisationController.cs
+++ NICE.Identity.Authorisation.WebAPI/Controllers/OrganisationController.cs
@@ -29,33 +29,33 @@ namespace NICE.Identity.Authorisation.WebAPI.Controllers
         /// </summary>
         /// <param name="organisation"></param>
         /// <returns></returns>
-        //[HttpPost("")]
-        //[ProducesResponseType(typeof(Organisation), StatusCodes.Status201Created)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //[ProducesResponseType(StatusCodes.Status409Conflict)]
-        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        //[Consumes("application/json")]
-        //[Produces("application/json")]
-        //public IActionResult CreateRole(Organisation organisation)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        var serializableModelState = new SerializableError(ModelState);
-        //        var modelStateJson = JsonConvert.SerializeObject(serializableModelState);
-        //        _logger.LogError($"Invalid model for create role", modelStateJson);
-        //        return BadRequest(new ProblemDetails { Status = 400, Title = "Invalid model for create role" });
-        //    }
+        [HttpPost("")]
+        [ProducesResponseType(typeof(Organisation), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult CreateOrganisation(Organisation organisation)
+        {
+            if (!ModelState.IsValid)
+            {
+                var serializableModelState = new SerializableError(ModelState);
+                var modelStateJson = JsonConvert.SerializeObject(serializableModelState);
+                _logger.LogError($"Invalid model for create organisation", modelStateJson);
+                return BadRequest(new ProblemDetails { Status = 400, Title = "Invalid model for create organisation" });
+            }
 
-        //    try
-        //    {
-        //        var createdRole = _rolesService.CreateRole(role);
-        //        return Created($"/role/{createdRole.RoleId.ToString()}", createdRole);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return StatusCode(500, new ProblemDetails { Status = 500, Title = e.Message, Detail = e.InnerException?.Message });
-        //    }
-        //}
+            try
+            {
+                var createdOrganisation = _organisationService.CreateOrganisation(organisation);
+                return Created($"/organsation/{createdOrganisation.OrganisationId.ToString()}", createdOrganisation);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new ProblemDetails { Status = 500, Title = e.Message, Detail = e.InnerException?.Message });
+            }
+        }
 
         /// <summary>
         /// get list of all organisation
@@ -65,7 +65,7 @@ namespace NICE.Identity.Authorisation.WebAPI.Controllers
         [ProducesResponseType(typeof(List<Organisation>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Produces("application/json")]
-        public IActionResult GetRoles()
+        public IActionResult GetOrganisations()
         {
             try
             {
@@ -84,7 +84,7 @@ namespace NICE.Identity.Authorisation.WebAPI.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(Role), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Organisation), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Produces("application/json")]
@@ -98,6 +98,61 @@ namespace NICE.Identity.Authorisation.WebAPI.Controllers
                     return Ok(organisation);
                 }
                 return NotFound(new ProblemDetails { Status = 404, Title = "Organisation not found" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new ProblemDetails { Status = 500, Title = $"{e.Message}" });
+            }
+        }
+
+        /// <summary>
+        /// update organisation with id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="organisation"></param>
+        /// <returns></returns>
+        [HttpPatch("{id}", Name = "UpdateOrganisationPartial")]
+        [HttpPut("{id}", Name = "UpdateOrganisation")]
+        [ProducesResponseType(typeof(Organisation), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult UpdateOrganisation(int id, Organisation organisation)
+        {
+            if (!ModelState.IsValid)
+            {
+                var serializableModelState = new SerializableError(ModelState);
+                var modelStateJson = JsonConvert.SerializeObject(serializableModelState);
+                _logger.LogError($"Invalid model for update organisation {modelStateJson}");
+                return BadRequest(new ProblemDetails { Status = 400, Title = "Invalid model for update organisation" });
+            }
+
+            try
+            {
+                return Ok(_organisationService.UpdateOrganisation(id, organisation));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new ProblemDetails { Status = 500, Title = $"{e.Message}" });
+            }
+        }
+
+        /// <summary>
+        /// delete organsation with id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json")]
+        public IActionResult DeleteOrganisation(int id)
+        {
+            try
+            {
+                _organisationService.DeleteOrganisation(id);
+                return Ok();
             }
             catch (Exception e)
             {
