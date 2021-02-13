1e3c86dc977b0f73fbe316d580a3fbb1ed623f48
John Williams <jcwilliams@gmail.com>
2021-02-13 17:26:04 +0000

    IDAM-420 - adding a .net standard test client.

---
+++ TestClients/NICE.Identity.TestClient.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NICE.Identity.TestClient.NETStandard2._0;
+
+namespace NICE.Identity.TestClient.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("About to spin up the .NET standard 2.0 class library");
+
+			var standardTester = new StandardTester();
+
+			standardTester.Go();
+
+			Console.WriteLine("Finished.");
+		}
+	}
+}
