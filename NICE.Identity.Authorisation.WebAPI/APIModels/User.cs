087e963fb40e56700ed3cd9a8c6cf2f47774f360
John Williams <jcwilliams@gmail.com>
2021-11-23 22:51:20 +0000

    IDAM-268 - removing a comment

--- NICE.Identity.Authorisation.WebAPI/APIModels/User.cs
+++ NICE.Identity.Authorisation.WebAPI/APIModels/User.cs
@@ -10,26 +10,6 @@ namespace NICE.Identity.Authorisation.WebAPI.ApiModels
         public User()
         {
         }
-
-        //public User(int userId, string nameIdentifier, string firstName, string lastName, string email, bool allowContactMe, bool hasVerifiedEmailAddress, bool isLockedOut, DateTime? initialRegistrationDate, DateTime? lastLoggedInDate, bool isStaffMember, bool acceptedTerms, bool isMigrated, bool isInAuthenticationProvider, IEnumerable<int> hasAccessToWebsiteIds)
-        //{
-        //    UserId = userId;
-        //    NameIdentifier = nameIdentifier;
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //    AllowContactMe = allowContactMe;
-        //    InitialRegistrationDate = initialRegistrationDate;
-        //    LastLoggedInDate = lastLoggedInDate;
-        //    HasVerifiedEmailAddress = hasVerifiedEmailAddress;
-        //    EmailAddress = email;
-        //    IsLockedOut = isLockedOut;
-        //    IsStaffMember = isStaffMember;
-        //    AcceptedTerms = acceptedTerms;
-        //    IsMigrated = isMigrated;
-        //    IsInAuthenticationProvider = isInAuthenticationProvider;
-        //    HasAccessToWebsiteIds = hasAccessToWebsiteIds;
-        //}
-
         public User(DataModels.User user)
         {
             UserId = user.UserId;
